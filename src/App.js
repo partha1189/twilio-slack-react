c242abf6c896ef7ce90ff281ac2184ffa7b55430
Partha Sen <partha1189@gmail.com>
2022-01-03 19:40:32 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import Chat from "./components/Chat";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [emailWarning, setEmailWarning] = useState(false);
+
+  let history = useHistory();
+  function login() {
+    if (email) {
+      localStorage.setItem("email", email);
+      history.push("chat");
+    } else if (!email) {
+      setEmailWarning(true);
+    }
+  }
+
+  const updateEmail = (e) => {
+    setEmail(e);
+    setEmailWarning(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/:room" component={Chat} />
+      <Route path="/">
+        <div className="container">
+          <main className="main">
+            <h1 className="title">Welcome</h1>
+
+            <section className="loginSection">
+              <input
+                type="email"
+                onChange={(e) => updateEmail(e.target.value)}
+                placeholder="Email"
+              />
+              {emailWarning && (
+                <span className="warning">You need to enter your email.</span>
+              )}
+              <button onClick={login}>Continue</button>
+            </section>
+          </main>
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/components/Chat.js
@@ -0,0 +1,263 @@
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import ChatItem from "./ChatItem";
+import React, { useRef } from "react";
+import { useEffect, useState } from "react";
+const ChatAPI = require("twilio-chat");
+
+function Chat() {
+  const email = localStorage.getItem("email");
+  const room = window.location.pathname.split("/")[1];
+
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [channel, setChannel] = useState(null);
+  const [text, setText] = useState("");
+  // const [link, setLink] = useState(null);
+
+  // const messages = []
+
+  const roomsList = ["general"];
+  let scrollDiv = useRef(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    console.log(scrollDiv);
+    let token = "";
+
+    if (!email) {
+      history.push("/");
+    }
+
+    setLoading(true);
+
+    try {
+      token = await getToken(email);
+      //   console.log(token)
+    } catch {
+      throw new Error("Unable to get token, please reload this page");
+    }
+
+    const client = await ChatAPI.Client.create(token);
+
+    client.on("tokenAboutToExpire", async () => {
+      const token = await getToken(email);
+      client.updateToken(token);
+    });
+
+    client.on("tokenExpired", async () => {
+      const token = await getToken(email);
+      client.updateToken(token);
+    });
+
+    client.on("channelJoined", async (channel) => {
+      // getting list of all messages since this is an existing channel
+      const newMessages = await channel.getMessages();
+      console.log("AFTER join messages:", newMessages);
+      // messages.push(newMessages.items || [])
+      setMessages(newMessages.items || []);
+      // scrollToBottom();
+    });
+
+    try {
+      const channel = await client.getChannelByUniqueName(room);
+      console.log(channel);
+      joinChannel(channel);
+      setChannel(channel);
+    } catch (err) {
+      try {
+        const channel = await client.createChannel({
+          uniqueName: room,
+          friendlyName: room,
+        });
+
+        joinChannel(channel);
+        //   console.log("channel:"+channel)
+      } catch {
+        throw new Error("Unable to create channel, please reload this page");
+      }
+    }
+  }, [email, room]);
+
+  const updateText = (e) => setText(e);
+
+  const joinChannel = async (channel) => {
+    if (channel.channelState.status !== "joined") {
+      await channel.join();
+    }
+
+    setChannel(channel);
+    setLoading(false);
+
+    channel.on("messageAdded", function (message) {
+      handleMessageAdded(message);
+    });
+    //    scrollToBottom();
+  };
+
+  let history = useHistory();
+
+  const changeRoom = (room) => {
+    history.push(room);
+  };
+
+  const getToken = async (email) => {
+    const response = await axios.get(`http://localhost:4000/token/${email}`);
+    const { data } = response;
+    return data.token;
+  };
+
+  const handleMessageAdded = async (message) => {
+    // setMessages((messages) => [...messages, message]);
+    // messages.push(message)
+    console.log(message);
+    console.log("messages:", messages);
+    ///////////////////////////////////
+    const txt = message.state.body;
+    console.log("INCLUDES LISTEN", txt);
+    if (txt.includes("listen")) {
+      const arr = txt.split(" ");
+      arr.shift();
+      console.log(arr);
+      const response = await axios.get(`search?term=${arr.join("+")}`);
+      const { data } = response;
+      console.log("APPLE DATA:", data.results[0]);
+      // setLink(data.results[0].trackViewUrl);
+      message.link = data.results[0].trackViewUrl;
+      message.state.link = data.results[0].trackViewUrl;
+      // channel.sendMessage(String(data.results[0].trackViewUrl).trim());
+
+      // Slack
+      // const json = JSON.stringify({
+      //   text: String(data.results[0].trackViewUrl).replace(
+      //     "https://music.apple.com",
+      //     "http://localhost:3001"
+      //   ),
+      // });
+
+      // const json = JSON.stringify({
+      //   text: String(data.results[0].trackViewUrl),
+      //   unfurl_links: true,
+      //   attachments: [
+      //     {
+      //       text: data.results[0].artistName,
+      //       image_url: data.results[0].artworkUrl100,
+      //     },
+      //   ],
+      //   unfurls: {
+      //     "https://example.com": {
+      //       preview: {
+      //         title: {
+      //           type: "plain_text",
+      //           text: "custom preview",
+      //         },
+      //         icon_url: data.results[0].artworkUrl100,
+      //       },
+      //     },
+      //   },
+      // });
+      const json = JSON.stringify({
+        text: String(data.results[0].trackViewUrl),
+        unfurl_links: true,
+        unfurls: {
+          "https://example.com": {
+            preview: {
+              title: {
+                type: "plain_text",
+                text: "custom preview",
+              },
+              icon_url: data.results[0].artworkUrl100,
+            },
+          },
+        },
+      });
+      const slackResponse = await axios.post(
+        `https://hooks.slack.com/services/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`,
+        json,
+        {
+          headers: {
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Headers":
+            //   "Origin, X-Requested-With, Content-Type, Accept",
+            // "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+            "Content-type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      console.log("SLACK", slackResponse);
+    }
+    setMessages((messages) => [...messages, message]);
+    //////////////////////////////////
+    scrollToBottom();
+  };
+
+  const scrollToBottom = () => {
+    const scrollHeight = scrollDiv.current.scrollHeight;
+    const height = scrollDiv.current.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    scrollDiv.current.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+  };
+
+  const sendMessage = () => {
+    if (text) {
+      console.log(String(text).trim());
+      setLoading(true);
+      channel.sendMessage(String(text).trim());
+      ///////////////
+
+      ////////////
+
+      setText("");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="chatScreen">
+      <div className="sidebar">
+        <h4>{email}</h4>
+        <h2>Rooms</h2>
+        {roomsList.map((room) => (
+          <p key={room} onClick={() => changeRoom(room)}>
+            {room}
+          </p>
+        ))}
+      </div>
+
+      <div className="chatContainer" ref={scrollDiv}>
+        <div className="chatHeader">
+          {room === "chat" ? "Choose A Room" : room}
+        </div>
+
+        <div className="chatContents">
+          {messages &&
+            room !== "chat" &&
+            messages.map((message) => (
+              <>
+                <ChatItem
+                  key={message.index}
+                  message={message}
+                  email={email}
+                  link={message.link}
+                />
+              </>
+            ))}
+        </div>
+
+        {room !== "chat" && (
+          <div className="chatFooter">
+            <input
+              type="text"
+              placeholder="Type Message"
+              onChange={(e) => updateText(e.target.value)}
+              value={text}
+            />
+            <button onClick={sendMessage}>Send</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
